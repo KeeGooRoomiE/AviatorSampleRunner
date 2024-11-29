af435151fe746eaa9c9dc1eb0483fdc6e0d4f953
keegooroomie <spartan121@bk.ru>
2024-11-29 23:09:10 +0700

    ADD/ firebase plugin

--- AviatorProj/Assets/Scripts/GUI/SceneGameManager.cs
+++ AviatorProj/Assets/Scripts/GUI/SceneGameManager.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Firebase.Database;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneGameManager : MonoBehaviour
 {
+    public string mainScene = "MainGame";
+    public string webViewScene = "WebViewScene";
+
+    void Start()
+    {
+        FirebaseDatabase.DefaultInstance.GetReference("webview").GetValueAsync().ContinueWith(task =>
+        {
+            if (task.IsCompleted && task.Result.Exists)
+            {
+                bool showWebView = bool.Parse(task.Result.Value.ToString());
+                SceneManager.LoadScene(showWebView ? webViewScene : mainScene);
+            }
+        });
+    }
     public void OpenGameScene()
     {
         SceneManager.LoadScene("GameScene"); 
     }
+    
 }
